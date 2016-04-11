31294d9727008d454e214758f3fa32ae476e09f9
ZhuangEr <huangyu.wuhan@gmail.com>
2016-04-11 17:07:02 -0400

    initialize with storm code

---
+++ Storm-code/src/jvm/yu/storm/TweetTopology.java
@@ -0,0 +1,85 @@
+package yu.storm;
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+import backtype.storm.utils.Utils;
+
+
+
+class TweetTopology
+{
+  public static void main(String[] args) throws Exception
+  {
+    // create the topology
+    TopologyBuilder builder = new TopologyBuilder();
+
+    /*
+     * In order to create the spout, you need to get twitter credentials
+     * If you need to use Twitter firehose/Tweet stream for your idea,
+     * create a set of credentials by following the instructions at
+     *
+     * https://dev.twitter.com/discussions/631
+     *
+     */
+    // now create the tweet spout with the credentials
+    // credential
+    TweetSpout tweetSpout = new TweetSpout(
+            "WXDgVgeJMwHEn0Z9VHDx5j93h",
+            "DgP9CsaPtG87urpNU14fZySXOjNX4j4v2PqmeTndcjjYBgLldy",
+            "3243813491-ixCQ3HWWeMsthKQvj5MiBvNw3dSNAuAd3IfoDUw",
+            "aHOXUB4nbhZv2vbAeV15ZyTAD0lPPCptCr32N0PX7OaMe"
+    );
+
+    // attach the tweet spout to the topology - parallelism of 1
+    builder.setSpout("tweet-spout", tweetSpout, 1);
+
+    // attach the parse tweet bolt using shuffle grouping
+ /*   builder.setBolt("parse-tweet-bolt", new ParseTweetBolt(), 10).shuffleGrouping("tweet-spout");*/
+    builder.setBolt("regex-bolt", new RegexBolt(), 10).shuffleGrouping("tweet-spout");
+    builder.setBolt("count-bolt", new CountBolt(), 10).fieldsGrouping("regex-bolt", new Fields("countryName"));
+    builder.setBolt("report-bolt", new ReportBolt(), 1).globalGrouping("count-bolt");
+
+
+    // create the default config object
+    Config conf = new Config();
+
+    // set the config in debugging mode
+    conf.setDebug(true);
+
+    if (args != null && args.length > 0) {
+
+      // run it in a live cluster
+
+      // set the number of workers for running all spout and bolt tasks
+      conf.setNumWorkers(3);
+
+      // create the topology and submit with config
+      StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+
+    } else {
+
+      // run it in a simulated local cluster
+
+      // set the number of threads to run - similar to setting number of workers in live cluster
+      conf.setMaxTaskParallelism(4);
+
+      // create the local cluster instance
+      LocalCluster cluster = new LocalCluster();
+
+      // submit the topology to the local cluster
+      cluster.submitTopology("tweet-word-count", conf, builder.createTopology());
+
+      // let the topology run for 300 seconds. note topologies never terminate!
+      Utils.sleep(300000000);
+
+      // now kill the topology
+      cluster.killTopology("tweet-word-count");
+
+      // we are done, so shutdown the local cluster
+      cluster.shutdown();
+    }
+  }
+}
---
+++ web/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, Response
+import redis
+# import pycountry
+
+app = Flask(__name__)
+r = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
+
+def event_stream():
+    pubsub = r.pubsub()
+    pubsub.subscribe('WordCountTopology')
+    for message in pubsub.listen():
+        print message
+        # print message['data'].split('DELIMITER')[3]
+        yield 'data: %s\n\n' % message['data']
+
+
+@app.route('/')
+@app.route('/map')
+def show_map():
+  #Basic d3 view = basic.html and app.js
+    return render_template("world_map.html")
+
+@app.route('/ca_map')
+def ca_map():
+	return render_template("ca_map.html")
+
+@app.route('/test')
+def test():
+    return render_template("test.html")
+
+@app.route('/stream')
+def stream():
+    return Response(event_stream(), mimetype="text/event-stream")
+
+if __name__ == '__main__':
+    app.run(threaded=True, host='0.0.0.0', debug=True)
