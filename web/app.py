a88d0048e13dda77b5c83c5e8c4fbcb81ee3f0b7
ZhuangEr <huangyu.wuhan@gmail.com>
2016-06-01 23:59:26 -0400

    build basic framework

--- web/app.py
+++ web/app.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*- 
 
-from flask import Flask, render_template, Response, request, jsonify, flash, g
+from flask import Flask, render_template, Response, request, jsonify, flash, g, redirect, url_for
 import redis
 import csv
 import os, sys
+import subprocess
 
 
 lib_path = os.path.abspath(os.path.join('tools'))
@@ -38,17 +39,91 @@ def kafka_stream():
 
 
 @app.route('/')
-@app.route('/component', methods=['GET'])
+@app.route('/component', methods=['GET', 'POST'])
 def component():
+    if request.method == 'POST':
+        collection = request.form.get('data-collection')
+        processing = request.form.get('data-processing')
+        visualization = request.form.get('data-visualization')
+        persistence = request.form.get('data-persistence')
+        
+        # data collection
+        kafka = KafkaClient("localhost:9092")
+        kafka_producer = SimpleProducer(kafka)
+        if collection == "user-timeline":
+            twitter_account = request.form.get('twitter-account')
+            # print twitter_account
+        elif collection == "query-by-text":
+            query_text = request.form.get('query-text')
+            # print query_text
+        elif collection == "query-by-location":
+            query_location = request.form.get('query-location')
+            # print query_location
+        elif collection == "favorite-list":
+            favorite_list = request.form.get('favorite-list')
+            # print favorite_list
+
+        if processing == "sentiment":
+            # subprocess.call(['storm', 'jar', 'jar/sentiment.jar', 'yu.storm.SentimentTopology'])
+            pass
+        elif processing == "tfidf":
+            # subprocess.call(['storm', 'jar', 'jar/tfidf.jar', 'yu.storm.TfidfTopology'])
+            pass
+        elif processing == "word-count":
+            pass
+        elif processing == "top-n":
+            pass
+
+        if visualization == "map":
+            return redirect(url_for('map'))
+        elif visualization == "line-graph":
+            return redirect(url_for('line'))
+        elif visualization == "radar-chart":
+            return redirect(url_for('radar'))
+        elif visualization == "pie-chart":
+            return redirect(url_for('pie'))
+        elif visualization == "word-cloud":
+            return redirect(url_for('word_cloud'))
+
+        if persistence == "user-timeline":
+            pass
+        elif persistence == "query-by-text":
+            pass
+
+
     return render_template("component.html")
 
+@app.route('/topology', methods=['GET'])
+def topology():
+    collection = request.form.get('data-collection')
+    processing = request.form.get('data-processing')
+    visualization = request.form.get('data-visualization')
+    persistence = request.form.get('data-persistence')
+    print collection, processing, visualization, persistence
+    return str(collection)
 
-@app.route('/word-cloud')
+@app.route('/visualization/map')
+def map():
+    return render_template("visualization/map.html")
+
+@app.route('/visualization/line')
+def line():
+    return render_template("visualization/line.html")
+
+@app.route('/visualization/radar')
+def radar():
+    return render_template("visualization/radar.html")
+
+@app.route('/visualization/pie')
+def pie():
+    return render_template("visualization/pie.html")
+
+@app.route('/visualization/word-cloud')
 def word_cloud():
-    return render_template("word-cloud.html")
+    return render_template("visualization/word-cloud.html")
 
 
-
+# all data collection send data through twitter topic
 @app.route('/_twitter_query')
 def twitter_query():
     kafka = KafkaClient("localhost:9092")
